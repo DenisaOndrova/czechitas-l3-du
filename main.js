133638f1f289ca4da4234ad85ae7a768b14bfd17
DenisaO <ondrovadenisa95@gmail.com>
2023-02-02 21:34:50 +0100

    ukol 3 - 2. část nedokončeno

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 // tady je místo pro náš program
+let jmenoPrijmeniUzivatele = document.querySelector('.jmenoPrijmeni')
+let vyplneneJmeno = prompt('Vyplňte Vaše jméno a příjmení:')
 
+jmenoPrijmeniUzivatele.innerHTML = vyplneneJmeno
 
+let narozeni = document.querySelector('.rokNarozeni')
+let vyplnenyRok = Number(prompt('Vyplňte Váš rok narození:'))
+let soucasnyRok = 2023
+
+narozeni.innerHTML = soucasnyRok - vyplnenyRok
+
+function obarveni(){
+    let barva = prompt('Jak se Vaše oblíbená barva řekne anglicky?')
+   
+}
 
 
