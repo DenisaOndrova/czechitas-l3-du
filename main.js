df809a1117e83182e8a405d24713731b78df48bd
DenisaO <ondrovadenisa95@gmail.com>
2023-02-07 20:21:10 +0100

    Upraveno - domácí úkol

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 // Ukol 1
-let jmenoPrijmeniUzivatele = document.querySelector('.jmenoPrijmeni')
-let vyplneneJmeno = prompt('Vyplňte Vaše jméno a příjmení:')
+let jmenoPrijmeniUzivatele = document.querySelector('.jmenoPrijmeni');
+let vyplneneJmeno = prompt('Vyplňte Vaše jméno a příjmení:');
 
-jmenoPrijmeniUzivatele.innerHTML = vyplneneJmeno
+jmenoPrijmeniUzivatele.innerHTML = vyplneneJmeno;
 
-let narozeni = document.querySelector('.rokNarozeni')
-let vyplnenyRok = Number(prompt('Vyplňte Váš rok narození:'))
-let soucasnyRok = 2023
+let narozeni = document.querySelector('.rokNarozeni');
+let vyplnenyRok = Number(prompt('Vyplňte Váš rok narození:'));
+let soucasnyRok = 2023;
 
-narozeni.innerHTML = soucasnyRok - vyplnenyRok
+narozeni.innerHTML = soucasnyRok - vyplnenyRok;
 
 //Ukol 2
 
-let barva = prompt('Jak se Vaše oblíbená barva řekne anglicky?')
+let barva = prompt('Jak se Vaše oblíbená barva řekne anglicky?');
 narozeni.style.color= barva;
 jmenoPrijmeniUzivatele.style.color = barva;
+
 
 
